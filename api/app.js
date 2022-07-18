23839aa41b63660aaa7dbcd87ab45ea67021af94
t_yagit <62111840+t-yagit@users.noreply.github.com>
2022-07-19 02:20:35 +0900

    create api.

---
+++ api/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+// new API route: GET /users, returning a list of users
+app.get('/', (request, response) => {
+  let res = {"api1": { id: 546, name: 'John' },
+  "api2": { id: 894, name: 'Mary' },
+  "api3": { id: 326, name: 'Jane' }}
+  if (request.query.id){
+    res = res[request.query.id]
+  }
+  response.json(
+    res
+  );
+});
+
+// DELETE user
+app.delete('/', (request, response) => {
+  response.json({ result: 'success' });
+});
+
+app.listen(3001, () => {});

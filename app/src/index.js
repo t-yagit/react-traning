12687bc8eae15bfab379da7b04ddf719488f9ccf
t_yagit <62111840+t-yagit@users.noreply.github.com>
2022-07-25 00:20:53 +0900

    fix separete dataAttrs.

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Article from "./components/Article"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const apis = document.querySelectorAll('div[data-js="api"]')
+let api_params = []
+apis.forEach(element => {
+  api_params.push(element.getAttribute('data-api-js'))
+});
+(async () => {
+  const response = await fetch(`http://localhost:3001/`, {mode: 'cors'})
+  const r = await response.json()
+  Object.keys(r).map((key)=>{
+    console.log(key)
+    const selecter = ReactDOM.createRoot(document.querySelector(`div[data-api-js=${key}]`));
+    selecter.render(
+      <React.StrictMode>
+        <Article
+         name={r[key].name}
+         id={r[key].id}
+        />
+      </React.StrictMode>
+    ); 
+  })
+})();

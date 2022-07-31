77f6ea37f2f8d10128e47fcff19c055d68de516c
t_yagit <62111840+t-yagit@users.noreply.github.com>
2022-07-31 22:10:12 +0900

    add naivety codes.

--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const subRoot1 = ReactDOM.createRoot(document.querySelector(`div[data-api-js="api1"]`))
+const subRoot2 = ReactDOM.createRoot(document.querySelector(`div[data-api-js="api2"]`))
+const subRoot3 = ReactDOM.createRoot(document.querySelector(`div[data-api-js="api3"]`))
+
+subRoot1.render(<App />)
+subRoot2.render(<App />)
+subRoot3.render(<App />)

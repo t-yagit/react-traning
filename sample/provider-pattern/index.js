be413a91f865923233420376673639034118504f
t-yagit <t-yagit@users.noreply.github.com>
2022-08-14 23:49:20 +0900

    add provider pattern in sample.

---
+++ sample/provider-pattern/index.js
@@ -0,0 +1,60 @@
+import React, { useState, createContext, useContext, useEffect } from "react";
+import ReactDOM from "react-dom";
+import moment from "moment";
+
+import "./styles.css";
+
+const CountContext = createContext(null);
+
+function Reset() {
+  const { setCount } = useCountContext();
+
+  return (
+    <div className="app-col">
+      <button onClick={() => setCount(0)}>Reset count</button>
+      <div>Last reset: {moment().format("h:mm:ss a")}</div>
+    </div>
+  );
+}
+
+function Button() {
+  const { count, setCount } = useCountContext();
+
+  return (
+    <div className="app-col">
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div>Current count: {count}</div>
+    </div>
+  );
+}
+
+function useCountContext() {
+  const context = useContext(CountContext);
+  if (!context)
+    throw new Error(
+      "useCountContext has to be used within CountContextProvider"
+    );
+  return context;
+}
+
+function CountContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <CountContext.Provider value={{ count, setCount }}>
+      {children}
+    </CountContext.Provider>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <CountContextProvider>
+        <Button />
+        <Reset />
+      </CountContextProvider>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
